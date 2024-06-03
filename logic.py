ec96cea03e24714075d2f63076e164395793435f
mnsmns48 <getzboy@list.ru>
2024-06-03 22:39:23 +0300

    working under logic

--- logic.py
+++ logic.py
@@ -10,6 +10,16 @@ async def scrape_resumes(link):
         browser = await run_browser(playwright)
         context = await browser.new_context()
         page = await context.new_page()
-        await page.goto(link)
-        print('here')
-        await asyncio.sleep(10)
+        await page.goto(link, wait_until="domcontentloaded")
+        # page.set_default_timeout(0)
+        last_pag = await page.locator("//a[@class='bloko-button']").nth(-2).inner_text()
+        count = 1
+        while count != int(last_pag) + 1:
+            if count == 1:
+                pass
+            else:
+                await page.goto(f"{link}&page={count-1}")
+            await asyncio.sleep(3)
+            print('page', count)
+            count += 1
+
