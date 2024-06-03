3c9b48b21367e75d323a3b2fcff8ac10025a7d4b
mnsmns48 <getzboy@list.ru>
2024-06-04 01:04:53 +0300

    working under logic

--- logic.py
+++ logic.py
@@ -1,8 +1,33 @@
-import asyncio
+import re
 
+from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright
 
 from browser import run_browser
+
+
+async def bs4_resumes(soup: BeautifulSoup) -> list:
+    result = list()
+    resumes_list = soup.find_all('div', {'data-qa': 'resume-serp__resume'})
+    for res in resumes_list:
+        title = res.find('a', {'data-qa': 'serp-item__title'})
+        excpirience = res.find('div', {'data-qa': 'resume-serp__resume-excpirience-sum'})
+        age = res.find('span', {'data-qa': 'resume-serp__resume-age'})
+        salary = age.find_next('div', {'class': ['bloko-text', 'bloko-text_strong']}) if age else None
+        status = res.find('div', {'class': re.compile(' tag_job-search-status--')})
+        prev_employment = res.find('div', {'data-qa': 'resume-serp_resume-item-content'})
+        result.append(
+            {
+                'title': title.span.text,
+                'age': age.text.strip() if age else None,
+                'link': f"https://hh.ru{title.get('href').split('?query=')[0]}",
+                'excpirience': excpirience.text.strip() if excpirience else None,
+                'salary': salary.text.strip() if salary and salary.text.strip() != 'Опыт работы' else None,
+                'status': status.text.strip() if status else None,
+                'prev_employment': prev_employment.text.strip() if prev_employment else None
+            }
+        )
+    return result
 
 
 async def scrape_resumes(link):
@@ -18,6 +43,14 @@ async def scrape_resumes(link):
             if count == 1:
                 pass
             else:
-                await page.goto(f'{link}&page={count - 1}, wait_until="domcontentloaded"')
-            print('page', count)
+                await page.locator("//a[@class='bloko-button']").nth(count - 1).click()
+            print('page:', count)
+            html = await page.locator("xpath=//main[@class='resume-serp-content']").inner_html()
+            soup = BeautifulSoup(markup=html, features='lxml')
+            data = await bs4_resumes(soup=soup)
+            for line in data:
+                for k, v in line.items():
+                    print(f"{k}: {v}")
+                print('\n>>>>>>>>>')
+            print('-------------------------')
             count += 1
